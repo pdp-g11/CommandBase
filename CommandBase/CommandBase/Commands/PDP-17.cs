538246b8c871f68e7a051f334a25928eee8133d5
Abdulloh-Alimov <abdullohalimov535@gmail.com>
2025-02-26 17:49:11 +0500

    A.Alimov change qildi

---
+++ CommandBase/CommandBase/Commands/PDP-17.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandBase.Commands
+{
+    internal class PDP_17
+    {
+        public void Execute()
+        {
+            Console.WriteLine("PDP-17 ");
+            Console.WriteLine("Alimov Abdulloh");
+        }
+        public int Sum(int a, int b,int c)
+        {
+            return a * b * c;
+        }
+
+    }
+}
