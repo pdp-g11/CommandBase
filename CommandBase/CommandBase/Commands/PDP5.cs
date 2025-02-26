baa413bb5f6b788f7c9a2c7b07598117224fd129
Adnabekova-Gulnoza <gulnoza19fxgc@gmail.com>
2025-02-26 17:47:48 +0500

    change qilindi

---
+++ CommandBase/CommandBase/Commands/PDP5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandBase.Commands
+{
+    internal class PDP5
+    {
+        public int Function1()
+        {
+            return 0;
+        }
+    }
+}
