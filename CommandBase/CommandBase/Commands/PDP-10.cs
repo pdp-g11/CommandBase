cebc8bb18762449ecb28965cbbe4981401ab7dce
Saidxoja404 <trashXshorts099@gmail.com>
2025-02-26 17:40:02 +0500

    Men ishimni bajardim

---
+++ CommandBase/CommandBase/Commands/PDP-10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandBase.Commands
+{
+    internal class PDP_10
+    {
+        public int HB ( int a, int b)
+        {
+            return a + b;
+        } 
+        public int H ( int a, int b)
+        {
+            return a - b;
+        }   
+
+    }
+}
